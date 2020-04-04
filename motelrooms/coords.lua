211bc6bbd0f753f9f26c1d6d900db0f4f4ceda87
TORCHIZM <60662894+TORCHIZM@users.noreply.github.com>
2020-04-04 12:19:09 +0300

    YMap of rooms

---
+++ motelrooms/coords.lua
@@ -0,0 +1,128 @@
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 1
+333.692 -208.6409 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 2
+321.9419 -209.4849 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 3
+323.4738 -209.2875 -46.56323
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 4
+334.0012 -209.4778 -46.5505
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 5
+322.5638 -225.1201 -45.90112
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 6
+333.2574 -223.6022 -46.39706
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 7
+343.781 -223.8917 -56.44944
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 8
+342.9912 -208.4175 -46.88467
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 9
+343.9401 -223.4072 -46.24038
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 10
+333.3889 -225.1058 -39.82808
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 11
+343.2563 -207.4216 -40.99081
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 12
+344.4826 -223.6022 -40.49163
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 13
+321.7946 -223.6022 -40.38594
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 14
+322.1974 -207.8561 -39.89917
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 15
+333.3889 -206.4238 -39.97617
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 16
+313.0329 -223.6022 -56.54506
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 17
+312.1377 -209.5261 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 18
+313.0851 -223.6022 -45.98107
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 19
+313.6053 -208.5451 -46.56748
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 20
+313.4265 -223.6022 -40.13078
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 21
+313.9552 -207.8284 -40.25175
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 22
+312.1943 -196.838 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 23
+312.1951 -196.318 -46.38351
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 24
+313.3249 -196.646 -40.40835
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 25
+322.2884 -196.5872 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 26
+321.9657 -196.0817 -46.58103
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 27
+321.8663 -195.9982 -40.22026
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 28
+343.7001 -235.7725 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 29
+343.3087 -235.7515 -46.18624
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 30
+344.1949 -235.5478 -40.12031
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 31
+333.3889 -236.466 -45.61179
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 32
+333.3889 -236.8408 -39.19715
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 33
+333.3889 -235.3406 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 34
+322.4941 -234.8657 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 35
+322.3566 -236.8754 -45.83131
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 36
+322.6902 -236.7551 -39.57561
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 37
+311.6494 -234.9746 -56.38824
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 38
+311.552 -235.2837 -39.99388
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 39
+311.509 -235.3568 -45.41493
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 40
+341.1144 -229.0787 42.88772
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 41
+333.3889 -223.6022 -56.79245
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 42
+322.4466 -223.3312 -56.57214
+
+hei_hw1_blimp_interior_v_motel_mp_newb11_milo_ - Copy 43
+333.3889 -223.29 -1.012336
